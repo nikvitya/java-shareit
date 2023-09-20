0cfe76174d9e72c67ededa971c280b549cef1a43
Vitaly Nikiforov <nikvitya@yandex.ru>
2023-09-20 10:37:49 +0300

    ТЗ 15. Привет , на проверку.

--- src/test/java/ru/practicum/shareit/exception/ErrorHandlerTests.java
+++ src/test/java/ru/practicum/shareit/exception/ErrorHandlerTests.java
@@ -4,15 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import ru.practicum.shareit.exception.handler.ErrorHandler;
 import ru.practicum.shareit.exception.handler.ErrorResponse;
-
-import javax.validation.ConstraintViolationException;
-import javax.validation.ValidationException;
-import java.util.HashSet;
-import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -23,6 +16,27 @@ public class ErrorHandlerTests {
     private ErrorHandler errorHandler;
 
     @Test
+    void handleUserNotBookedBeforeException() {
+        UserNotBookedBeforeException userNotBookedBeforeException = new UserNotBookedBeforeException("C");
+        ErrorResponse errorResponse = errorHandler.handleUserNotBookedBeforeException(userNotBookedBeforeException);
+        assertEquals("Пользователь не использовал в прошлом вещь.", errorResponse.getError());
+    }
+
+    @Test
+    void handleStatusAlreadyChangedExceptionTest() {
+        StatusAlreadyChangedException statusAlreadyChangedException = new StatusAlreadyChangedException("n");
+        ErrorResponse errorResponse = errorHandler.handleStatusAlreadyChangedException(statusAlreadyChangedException);
+        assertEquals("Статус был изменён ранее.", errorResponse.getError());
+    }
+
+    @Test
+    void handleEmailAlreadyExistExceptionTest() {
+        EmailAlreadyExistException emailAlreadyExistException = new EmailAlreadyExistException("/ ");
+        ErrorResponse errorResponse = errorHandler.handleEmailAlreadyExistException(emailAlreadyExistException);
+        assertEquals("Еmail уже существует.", errorResponse.getError());
+    }
+
+    @Test
     void handleNotFoundExceptionTest() {
         NotFoundException notFoundException = new NotFoundException("Not found");
         ErrorResponse errorResponse = errorHandler.handleNotFoundException(notFoundException);
@@ -30,48 +44,24 @@ public class ErrorHandlerTests {
     }
 
     @Test
-    void handleUserFoundExceptionTest() {
+    void handleUserNotFoundExceptionTest() {
         UserNotFoundException userNotFoundException = new UserNotFoundException("A");
         ErrorResponse errorResponse = errorHandler.handleUserNotFoundException(userNotFoundException);
         assertEquals("Пользователь не найден.", errorResponse.getError());
     }
 
     @Test
-    void handleItemFoundExceptionTest() {
-        ItemNotFoundException itemNotFoundException= new ItemNotFoundException("B");
+    void handleItemNotFoundExceptionTest() {
+        ItemNotFoundException itemNotFoundException = new ItemNotFoundException("B");
         ErrorResponse errorResponse = errorHandler.handleItemNotFoundException(itemNotFoundException);
         assertEquals("Предмет не найден.", errorResponse.getError());
     }
 
+
     @Test
-    void handleUserNotBookedBeforeException() {
-        UserNotBookedBeforeException userNotBookedBeforeException = new UserNotBookedBeforeException("C");
-        ErrorResponse errorResponse = errorHandler.handleUserNotBookedBeforeException(userNotBookedBeforeException);
-        assertEquals("Пользователь не использовал в прошлом вещь.", errorResponse.getError());
+    void handleExceptionTest() {
+        Exception exception = new Exception("C");
+        ErrorResponse errorResponse = errorHandler.handleException(exception);
+        assertEquals("Произошла непредвиденная ошибка.", errorResponse.getError());
     }
-//
-//    @Test
-//    void handleThrowableTest() {
-//        Throwable throwable = new Throwable("Что-то пошло не так.");
-//        ResponseEntity<Map<String, String>> response = errorHandler.handleThrowable(throwable);
-//        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
-//        assertEquals(response.getBody().get("error"), "Что-то пошло не так.");
-//    }
-//
-//    @Test
-//    void handleValidationExceptionTest() {
-//        ValidationException validationException = new ValidationException("Ошибка валидации.");
-//        ResponseEntity<Map<String, String>> response = errorHandler
-//                .handleValidationException(validationException);
-//        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
-//        assertEquals(response.getBody().get("error"), "Ошибка валидации.");
-//    }
-//
-//    @Test
-//    void handleConstraintViolationExceptionTest() {
-//        ConstraintViolationException validationException = new ConstraintViolationException(new HashSet<>());
-//        ResponseEntity<Map<String, String>> response = errorHandler
-//                .handleConstraintViolationException(validationException);
-//        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
-//    }
 }
